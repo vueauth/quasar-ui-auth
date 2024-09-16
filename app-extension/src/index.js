c2e397c6f03c22cd600e961841a8acf286b2cef2
Luke Diebold <luke@ldiebold.com>
2024-09-16 18:42:37 +1000

    allow new vite cli

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -16,7 +16,7 @@ module.exports = function (api) {
   api.compatibleWith('quasar', '^2.0.0')
 
   if (api.hasVite === true) {
-    api.compatibleWith('@quasar/app-vite', '^1.0.0-beta.0')
+    api.compatibleWith('@quasar/app-vite', '^1.x || ^2.x')
   } else {
     // api.hasWebpack === true
     api.compatibleWith('@quasar/app-webpack', '^3.0.0')
